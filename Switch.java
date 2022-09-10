f482cd7e3feffad0922e4c860d43a546f5204fad
stephen <113249744+stephendevapriyanabraham@users.noreply.github.com>
2022-09-10 20:19:11 +0530

    switch

---
+++ Switch.java
@@ -0,0 +1,98 @@
+package com.Switch;
+
+import java.util.Scanner;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		int day=1;
+		
+		
+		switch (day) {
+		
+		case 1:
+			
+			System.out.println("sunday");
+			
+			break;
+
+		case 2:
+			System.out.println("monday");
+			
+			break;
+		case 3:
+			System.out.println("tuesday");
+			
+			break;
+		case 4:
+			System.out.println("wednesday");
+			
+			break;
+		case 5:
+			System.out.println("thursday");
+			
+			break;
+		case 6:
+			System.out.println("friday");
+			
+			break;
+		case 7:
+			System.out.println("saturday");
+			break;
+		default:
+	           System.out.println("8 th day");
+			break;
+		}
+		
+	}
+}
+
+
+//
+//int a=6;
+//
+//Scanner y=new Scanner(System.in);
+//
+//System.out.println("G7 members");
+//
+//a=y.nextInt();
+//
+//switch (a) {
+//case 1:
+//	System.out.println("United states");
+//	break;
+//case 2:
+//	System.out.println("canada");
+//	break;
+//case 3:
+//	System.out.println("france");
+//	break;
+//case 4:
+//	System.out.println("germany");
+//	break;
+//case 5:
+//	System.out.println("italy");
+//	break;
+//case 6:
+//	System.out.println("japan");
+//	break;
+//case 7:
+//	System.out.println("uk");
+//	break;
+//default:
+//	System.out.println("Russia");
+//	break;
+
+
+
+
+
+
+
+
+
+
+
+
+
